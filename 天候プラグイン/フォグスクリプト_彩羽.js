98c535f8fe5f1e41c1ba3c0aad5cd4e638b70774
QBE256 <55350808+QBE256@users.noreply.github.com>
2022-03-08 10:23:24 +0900

    フォグスクリプトを軽量化

--- 天候プラグイン/フォグスクリプト_彩羽.js
+++ 天候プラグイン/フォグスクリプト_彩羽.js
@@ -67,6 +67,7 @@
   }
 
   ■バージョン履歴
+  2022/03/08  計算量やrootメソッドの呼び出し回数を減らして動作を軽量化
   2022/02/12  マップロード時に画像キャッシュを破棄する処理を追加（マップをまたいで天候が保持されてしまうバグを解消）
   2022/02/02  30FPSに対応
   2022/02/02  新規作成
@@ -92,14 +93,10 @@ CacheControl._FogPic = null;
 
 CacheControl.initialize_FogPic = function() {
   this._FogPic = null;
-}
+};
 
-CacheControl.get_FogPic = function() {
+CacheControl.get_FogPic = function(fog_info) {
   var pic;
-  var fog_info = MapLayer._getFogInfo();
-  if (!fog_info) {
-    return;
-  }
 
 	if (!this._FogPic) {
 		pic = root.getMaterialManager().createImage('FogImage', fog_info.FOG_IMAGE_NAME);
@@ -165,15 +162,23 @@ MapLayer.prepareMapLayer = function() {
     return;
   }
 
+  var pic = CacheControl.get_FogPic(fog_info);
+  var picSize = Math.max(pic.getWidth(), pic.getHeight());
+  var fogAreaWidth = root.getGameAreaWidth() + picSize * 2;
+  var fogAreaHeight = root.getGameAreaHeight() + picSize * 2;
+  //30FPSの場合は速さを2倍に
+  var highPerformanceCorrection = DataConfig.isHighPerformance() ? 1 : 2;
+  fog_info.FOG_SPEED_MAX *= highPerformanceCorrection;
+  fog_info.FOG_SPEED_MIN *= highPerformanceCorrection;
+  var scaleRange = fog_info.FOG_SCALE_MAX - fog_info.FOG_SCALE_MIN;
+  var angleRange = fog_info.FOG_ANGLE_MAX - fog_info.FOG_ANGLE_MIN;
+  var speedRange = fog_info.FOG_SPEED_MAX - fog_info.FOG_SPEED_MIN;
   for (var i = 0; i < fog_info.FOG_COUNT; i++) {
-    var pic = CacheControl.get_FogPic();
-    var picsize = Math.max(pic.getWidth(), pic.getHeight());
- 
-    this._FogX[i] = Math.random() * (root.getGameAreaWidth() + picsize * 2) - picsize;
-    this._FogY[i] = Math.random() * (root.getGameAreaHeight() + picsize * 2) - picsize;
-    this._FogScale[i] = Math.random() * (fog_info.FOG_SCALE_MAX - fog_info.FOG_SCALE_MIN) + fog_info.FOG_SCALE_MIN;
-    this._FogAngle[i] = Math.random() * (fog_info.FOG_ANGLE_MAX - fog_info.FOG_ANGLE_MIN) + fog_info.FOG_ANGLE_MIN;
-    this._FogSpeed[i] = Math.random() * (fog_info.FOG_SPEED_MAX - fog_info.FOG_SPEED_MIN) + fog_info.FOG_SPEED_MIN;
+    this._FogX[i] = Math.random() * fogAreaWidth - picSize;
+    this._FogY[i] = Math.random() * fogAreaHeight - picSize;
+    this._FogScale[i] = Math.random() * scaleRange + fog_info.FOG_SCALE_MIN;
+    this._FogAngle[i] = Math.random() * angleRange + fog_info.FOG_ANGLE_MIN;
+    this._FogSpeed[i] = Math.random() * speedRange + fog_info.FOG_SPEED_MIN;
   }
 };
 
@@ -183,43 +188,51 @@ MapLayer.moveMapLayer = function() {
   if (!fog_info) {
     return alias01.call(this);
   }
-  if (!this._isEnableLocalSwitchFog()) {
+  if (!this._isEnableLocalSwitchFog(fog_info)) {
     return alias01.call(this);
   }
 
-  var pic = CacheControl.get_FogPic();
+  var pic = CacheControl.get_FogPic(fog_info);
+  var picWidth = pic.getWidth();
+  var picHeight = pic.getHeight();
+  var fogAreaLeftEdge = -1 * picWidth;
+  var fogAreaTopEdge = -1 * picHeight;
+  var fogAreaRightEdge = picWidth + root.getGameAreaWidth();
+  var fogAreaBottomEdge =  picHeight + root.getGameAreaHeight();
+
   for (var i = 0; i < fog_info.FOG_COUNT; i++) {
-
-    var speed = this._FogSpeed[i];
-    //30FPSの場合は速さを2倍に
-    if (!DataConfig.isHighPerformance()) {
-      speed *= 2;
-		}
-
-    this._FogX[i] += Math.cos(this._FogAngle[i]) * speed;
-    this._FogY[i] += Math.sin(this._FogAngle[i]) * speed;
-  
     this._FogAngle[i] += (Math.random() - 0.5) * fog_info.FOG_ANGLE_VAR;
-    this._FogAngle[i] = Math.max(this._FogAngle[i], fog_info.FOG_ANGLE_MIN);
-    this._FogAngle[i] = Math.min(this._FogAngle[i], fog_info.FOG_ANGLE_MAX);
+
+    if (this._FogAngle[i] > fog_info.FOG_ANGLE_MAX) {
+      this._FogAngle[i] = fog_info.FOG_ANGLE_MAX;
+    }
+    else if (this._FogAngle[i] < fog_info.FOG_ANGLE_MIN) {
+      this._FogAngle[i] = fog_info.FOG_ANGLE_MIN;
+    }
 
     this._FogSpeed[i] += (Math.random() - 0.5) * fog_info.FOG_SPEED_VAR;
-    this._FogSpeed[i] = Math.max(this._FogSpeed[i], fog_info.FOG_SPEED_MIN);
-    this._FogSpeed[i] = Math.min(this._FogSpeed[i], fog_info.FOG_SPEED_MAX);
+    if (this._FogSpeed[i] > fog_info.FOG_SPEED_MAX) {
+      this._FogSpeed[i] = fog_info.FOG_SPEED_MAX;
+    }
+    else if (this._FogSpeed[i] < fog_info.FOG_SPEED_MIN) {
+      this._FogSpeed[i] = fog_info.FOG_SPEED_MIN;
+    }
+    
+    this._FogX[i] += Math.cos(this._FogAngle[i]) * this._FogSpeed[i];
+    this._FogY[i] += Math.sin(this._FogAngle[i]) * this._FogSpeed[i];
 
     // 画面外に出たら、反対側にワープ
-    var picsize = Math.max(pic.getWidth(), pic.getHeight());
-    if (this._FogX[i] > root.getGameAreaWidth() + picsize) {
-      this._FogX[i] = -1 * picsize;
+    if (this._FogX[i] > fogAreaRightEdge) {
+      this._FogX[i] = fogAreaLeftEdge;
     }
-    if (this._FogX[i] < - picsize) {
-      this._FogX[i] = root.getGameAreaWidth() + picsize;
+    else if (this._FogX[i] < fogAreaLeftEdge) {
+      this._FogX[i] = fogAreaRightEdge;
     }
-    if (this._FogY[i] > root.getGameAreaHeight() + picsize) {
-      this._FogY[i] = -1 * picsize;
+    if (this._FogY[i] > fogAreaBottomEdge) {
+      this._FogY[i] = fogAreaTopEdge;
     }
-    if (this._FogY[i] < - picsize) {
-      this._FogY[i] = root.getGameAreaHeight() + picsize;
+    else if (this._FogY[i] < fogAreaTopEdge) {
+      this._FogY[i] = fogAreaBottomEdge;
     }
   }
 
@@ -234,15 +247,16 @@ MapLayer.drawUnitLayer = function() {
   if (!fog_info) {
     return;
   }
-  if (!this._isEnableLocalSwitchFog()) {
+  if (!this._isEnableLocalSwitchFog(fog_info)) {
     return;
   }
 
-  var pic = CacheControl.get_FogPic();
+  var pic = CacheControl.get_FogPic(fog_info);
+  var AngleCorrection = 180 / Math.PI;
   for (var i = 0; i < fog_info.FOG_COUNT; i++) {
     pic.draw(this._FogX[i], this._FogY[i]);
     pic.setScale(this._FogScale[i]);
-    pic.setDegree(this._FogAngle[i] * 180 / Math.PI);
+    pic.setDegree(this._FogAngle[i] * AngleCorrection);
   }
 };
 
@@ -262,11 +276,9 @@ MapLayer._getFogInfo = function() {
 	return fog_info;
 };
 
-MapLayer._isEnableLocalSwitchFog = function() {
-  var currentMapInfo = root.getCurrentSession().getCurrentMapInfo();
-  var fog_info = currentMapInfo.custom.iroha_fog;
-
+MapLayer._isEnableLocalSwitchFog = function(fog_info) {
   if (fog_info.SWITCH_ID != null) {
+    var currentMapInfo = root.getCurrentSession().getCurrentMapInfo();
     var localSwitchTable = currentMapInfo.getLocalSwitchTable();
     var switchIndex = localSwitchTable.getSwitchIndexFromId(fog_info.SWITCH_ID);
     if (!localSwitchTable.isSwitchOn(switchIndex)) {
@@ -278,4 +290,3 @@ MapLayer._isEnableLocalSwitchFog = function() {
 }
 
 })();
-
