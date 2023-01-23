9010f76c078d86bd684c91caaa59f42ff904f0f7
iroha-code <for.magia.game@gmail.com>
2023-01-23 21:54:34 +0900

    立ち絵生成

--- キャラクリエイト/キャラクリエイト_立ち絵/04_スクリプト：立ち絵生成.js
+++ キャラクリエイト/キャラクリエイト_立ち絵/04_スクリプト：立ち絵生成.js
@@ -1,19 +1,30 @@
 /*--------------------------------------------------------------------------
 
-スクリプト：立ち絵書き換え
+スクリプト：立ち絵生成
 
 ■概要
 　イベントコマンドで「スクリプトの実行 > コード実行」を指定し、以下のように記述してください。
 　さらに，「オリジナルデータ」タブで，立ち絵を書き換えるユニットを設定してください。
-　(すでに立ち絵が生成されているキャラクターでないと正常に動作しません)
 ------------
-var categoryName = '目'; 
-var picName = 'ウインク.png';
-setCharaIllustMaterialPic(categoryName, picName);
+例) 立ち絵を構成するデータが以下のようになっている場合
+var CHARACTER_CREATE_MATERIAL_ARRAY = [
+	'目',
+	'口',
+	'本体',
+	'後髪'
+];
+
+// 「コード実行」に記載する内容
+var picName1 = 'ウインク.png';
+var picName2 = 'にっこり.png';
+var picName3 = '肌色.png'
+var picName4 = '後ろ髪';
+var picNameArr = [picName1, picName2, picName3, picName4];
+setCharaIllustMaterialPicArr(picNameArr);
 ------------
 
 ■バージョン履歴
-　2023/01/19  新規作成
+　2023/01/23  新規作成
 
 ■対応バージョン
 　SRPG Studio Version:1.267
@@ -28,18 +39,23 @@ setCharaIllustMaterialPic(categoryName, picName);
 
 --------------------------------------------------------------------------*/
 
-var setCharaIllustMaterialPic = function(categoryName, picName) {
+var setCharaIllustMaterialPicArr = function(picNameArr) {
 	var unit = root.getEventCommandObject().getOriginalContent().getUnit();
-
-	if (unit.custom.charaIndexArr) {
-		var categoryIndex = CHARACTER_CREATE_MATERIAL_ARRAY.indexOf(categoryName);
+	var charaIndexArr = [];
+	for (var j = 0; j < picNameArr.length; j++) {
+		var picName = picNameArr[j];
+		var categoryName = CHARACTER_CREATE_MATERIAL_ARRAY[j];
 		var count = root.getMaterialManager().getMaterialCount(categoryName);
 		for (var i = 0; i < count; i++) {
 			var name = root.getMaterialManager().getMaterialName(i, categoryName);
-
 			if (name === picName) {
-				unit.custom.charaIndexArr[categoryIndex] = i;
+				charaIndexArr.push(i);
 			}
 		}
+	}
+
+	// 正常にすべての画像が見つかった場合のみ、登録
+	if (charaIndexArr.length === CHARACTER_CREATE_MATERIAL_ARRAY.length) {
+		unit.custom.charaIndexArr = charaIndexArr;
 	}
 }
